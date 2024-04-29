b72aa958db118e55f2b0e6cc6636d675a74a79a2
roberromero <robertoromerodeveloper@gmail.com>
2024-04-29 20:25:17 +0100

    error views and post form completed

---
+++ routes.php
@@ -0,0 +1,12 @@
+<?php
+
+//associative array in PHP
+return [
+  '/' => '/blog_model_php/',
+  '/index' => '/blog_model_php/index.php/index',
+  '/contact' => '/blog_model_php/index.php/contact',
+  '/about' => '/blog_model_php/index.php/about',
+  '/posts'=> '/blog_model_php/index.php/posts',
+  '/post'=> '/blog_model_php/index.php/post',
+  '/posts-create'=>'/blog_model_php/index.php/posts-create'
+];
--- views/posts-create.view.php
+++ views/posts-create.view.php
@@ -3,12 +3,58 @@
 <?php require('partials/banner.php') ?>
 <main>
   <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-    <form method="post">
+    <!--<form method="post">
       <input type="text" name="title" value="<?= $_POST['title'] ?? '' ?>">
-      <span><?= $errors['title'] ?? '' ?></span><br>
+      <p class="text-red-500 text-sm"><?= $errors['title'] ?? '' ?></p><br>
       <input type="text"  name="description" value="<?= $_POST['description'] ?? '' ?>"><br><br>
+      <p class="text-red-500 text-sm"><?= $errors['description'] ?? '' ?></p><br>
       <input type="submit" value="Submit">
-    </form> 
+    </form> -->
+
+    <!--
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+    ],
+  }
+  ```
+-->
+<form method="post">
+  <div class="space-y-12">
+    <div class="border-b border-gray-900/10 pb-12">
+      <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+        <div class="sm:col-span-4">
+          <label for="title" class="block text-sm font-medium leading-6 text-gray-900">Title</label>
+          <div class="mt-2">
+            <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+              <input type="text" name="title" value="<?= $_POST['title'] ?? '' ?>" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Type your title...">
+            </div>
+            <p class="text-red-500 mt-3 text-sm leading-6"><?= $errors['title'] ?? '' ?></p><br>
+          </div>
+        </div>
+
+        <div class="col-span-full">
+          <label for="description" class="block text-sm font-medium leading-6 text-gray-900">Description</label>
+          <div class="mt-2">
+            <textarea name="description" rows="3" class="block w-full rounded-md border-0 py-1.5 pl-1 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"><?= $_POST['description'] ?? '' ?></textarea>
+          </div>
+          <p class="text-red-500 mt-3 text-sm leading-6"><?= $errors['description'] ?? '' ?></p><br>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="mt-6 flex items-center justify-end gap-x-6">
+    <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+  </div>
+</form>
+
   </div>
 </main>
 <?php require('partials/footer.php') ?>
